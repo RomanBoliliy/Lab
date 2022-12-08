7bc0c1f0e594facc073e8f82ae019276ff7027e3
RomanBoliliy <21fi.r.bolilyi@std.npu.edu.ua>
2022-12-08 19:27:34 +0200

    Added lab 3 cross platform programming

---
+++ MotherClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace lab3_KPP
+{
+     class MotherClass
+    {
+        
+        protected static string Password = "123";
+        protected static string Login = "qwe";
+
+        public void Refreshing() { Password = "0000"; Login = "0000"; }
+       
+    }
+}
